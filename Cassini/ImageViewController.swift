5946cfedca1125a812eb21c47d58adddaa5a066c
Michael Dardis <dardismm@gmail.com>
2018-08-13 17:08:24 -0400

    added new files, updated view controller

--- Cassini/ImageViewController.swift
+++ Cassini/ImageViewController.swift
@@ -9,7 +9,58 @@
 import UIKit
 
 class ImageViewController: UIViewController {
-
     
-
+    //this is my model
+    var imageURL: URL? {
+        didSet{
+            imageView.image = nil
+            if view.window != nil { //am I on screen? if so, then fetch image
+                fetchImage()
+            }
+        }
+    }
+    
+    //at this point, I know I'm on screen
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if imageView.image == nil {
+            fetchImage()
+        }
+    }
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    private func fetchImage() {
+        if let url = imageURL {
+            let urlContents = try? Data(contentsOf: url) //return nil if fails
+            if let imageData = urlContents {
+                imageView.image = UIImage(data: imageData)
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if imageURL == nil {
+            imageURL = DemoURLs.stanford
+        }
+    }
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
